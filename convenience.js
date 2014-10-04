b7ecbb9e76be648009420cae8fc2661dfe0f11ab
asvarga <alexander_varga@brown.edu>
2014-10-03 20:19:11 -0400

    how convenient

---
+++ convenience.js
@@ -0,0 +1,25 @@
+
+function log(x) {
+	console.log(x);
+}
+
+function get(o, key, def) {
+	return (key in o) ? o[key] : def;
+}
+
+function any(O) {
+	for (var k in O) {
+	    return O[k];
+	}
+}
+
+function isString(x) {
+	return (typeof x == 'string' || x instanceof String);
+}
+
+function rgb(r, g, b) {
+	return 'rgb('+r+', '+g+', '+b+')'
+}
+
+
+
